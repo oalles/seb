e9e71f7eabaa9f6e33987e17b83547ce88dbd1e9
oalles <oalles@neivi.es>
2016-01-27 16:02:32 +0100

    Polished

--- src/main/java/es/neivi/seb/configuration/EnableSEB.java
+++ src/main/java/es/neivi/seb/configuration/EnableSEB.java
@@ -8,11 +8,8 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.Async;
 
 import es.neivi.smb.annotation.EnableSMB;
-import es.neivi.smb.annotation.RootMessageEntity;
-import es.neivi.smb.annotation.SMBConfiguration;
 
 /**
  * Enables Simple Mongo Broadcaster capability, to be used on @
@@ -20,22 +17,10 @@ import es.neivi.smb.annotation.SMBConfiguration;
  *
  * <pre class="code">
  * &#064;Configuration
- * &#064;EnableSMB(consumerId=#{env.getRequiredProperty("smb.consumer_id")})
+ * &#064;EnableSEB
  * public class AppConfig {
- * 	&#064;Bean
- * 	public MyAsyncBean asyncBean() {
- * 		return new MyAsyncBean();
- * 	}
  * }
  * </pre>
- *
- * where {@code MyAsyncBean} is a user-defined type with one or methods
- * annotated with @{@link Async} (or any custom annotation specified by the
- * {@link #annotation()} attribute).
- *
- * @see RootMessageEntity
- * @see SMBConfigurer
- * @see SMBConfiguration
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
